bd0102668c3e935c34726ea8429b5917d675963e
Joshua Koshy <joshuavkoshy@gmail.com>
2026-03-31 10:13:23 -0500

    return disaster id

--- backend/app.py
+++ backend/app.py
@@ -34,4 +34,4 @@ def get_stats(disaster_id: str | None = None):
     num_minor_damage = 0
     num_major_damage = 0
     num_severe_damage = 0
-    return {"accuracy": accuracy, "num_no_damage": num_no_damage, "num_minor_damage": num_minor_damage, "num_major_damage": num_major_damage, "num_severe_damage": num_severe_damage}
+    return {"disaster_id": disaster_id, "accuracy": accuracy, "num_no_damage": num_no_damage, "num_minor_damage": num_minor_damage, "num_major_damage": num_major_damage, "num_severe_damage": num_severe_damage}
